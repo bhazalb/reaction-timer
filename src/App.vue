d2c12c6204fa00cbf40053786c1e9a727054855c
bhazalb <bayramhazal030@gmail.com>
2023-10-25 20:21:15 +0300

    Finish

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
  
- <h1>Hello </h1>
+ <h1>Let's play a game </h1>
  <button @click="start" :disabled="isPlaying">Play</button>
 <Block  v-if="isPlaying" :delay="delay"  @end="endGame">
 </Block>
-<p> Reaction time : {{ score }} ms</p>
+<p v-if="showResults"> Reaction time : {{ score }} ms</p>
+<Results v-if="showResults" :score="score"/>
 </template>
 
 <script>
  
  import Block from './components/Block.vue'
+ import Results from './components/Results.vue'
 
 export default {
   name: 'App',
-  components: { Block },
+  components: { Block , Results },
   data() {
     return {
       isPlaying: false,
@@ -26,11 +28,12 @@ export default {
     start(){
       this.delay = 2000 + Math.random() * 5000
       this.isPlaying = true
-     
+      this.showResults = false
     },
     endGame(reactionTime){
      this.score= reactionTime 
      this.isPlaying = false
+     this.showResults= true
     }
   }
 }
@@ -44,5 +47,20 @@ export default {
   text-align: center;
   color: #041627;
   margin-top: 60px;
+}
+button{
+  background: rgb(120, 115, 124);
+  color: #ffffff;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+}
+button:hover{
+  background-color: rgb(103, 23, 168);
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,33 @@
+<template>
+<p> Reaction Time: {{ score }}</p>
+<p class="rank">{{ rank }} </p>
+</template>
+
+<script>
+ export default{
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if(this.score <250){
+            this.rank = 'Cool'
+        } else if(this.score <400){
+            this.rank = 'ehh :('
+        }else{
+            this.rank= 'slowly :((('
+        }
+    },
+ }
+
+</script>
+<style>
+.rank {
+font-size: 1.4rem;
+color: deeppink;
+font-weight: bold;
+}
+
+</style>
